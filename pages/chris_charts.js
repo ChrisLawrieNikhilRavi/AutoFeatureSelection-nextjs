210c1671791752fe71bbc3e1fc86ebf1d319f6bf
Chris Lawrie <clawrie2020@gmail.com>
2023-01-26 20:59:40 -0500

    first commit

---
+++ pages/chris_charts.js
@@ -0,0 +1,35 @@
+import { FadeContainer } from "@/content/FramerMotionVariants";
+import { motion } from "framer-motion";
+import Metadata from "@/components/MetaData";
+import pageMeta from "@/content/meta";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+  let { url } = router.query;
+
+  return (
+    <>
+      <Metadata
+        description={pageMeta.preprocess.description}
+        previewImage={pageMeta.preprocess.image}
+        keywords={pageMeta.preprocess.keywords}
+      />
+
+      {/* Following is the new Code */}
+      <div className="relative dark:bg-darkPrimary dark:text-gray-100 max-w-4xl 2xl:max-w-5xl 3xl:max-w-7xl mx-auto">
+        <motion.section
+          initial="hidden"
+          whileInView="visible"
+          variants={FadeContainer}
+          viewport={{ once: true }}
+          className="grid place-content-center py-20  min-h-screen"
+        >
+          <div className="w-full relative mx-auto flex flex-col items-center gap-10">
+            Front end sandbox for chris! :)
+          </div>
+        </motion.section>
+      </div>
+    </>
+  );
+}
